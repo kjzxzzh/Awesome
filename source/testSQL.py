afe178f4f6f60fe6e81985fb4dcd1b949bb4f7a5
asd <qwe>
2017-03-02 20:35:48 +0800

    change dir

--- source/testSQL.py
+++ source/testSQL.py
@@ -1,10 +1,10 @@
 import asyncio
 import logging;logging.basicConfig(level=logging.INFO)
-import orm
-from Model import User
+from source import orm
+from source.Model import User
 
 async def test():
-    await orm.create_pool(loop=loop,host='127.0.0.1', port=3306, user='root', password='123456', db='awesome')
+    await orm.create_pool(loop=loop, host='127.0.0.1', port=3306, user='root', password='123456', db='awesome')
     u = User(name='xiaopeng', email='chenhao8@qq.com', passwd='1234567890', image='about:blank')
     await u.save()
     u = User(name='xiaopeng23', email='chenhao8@23qq.com', passwd='123456789230', image='about:bla23nk')
